9a0d37c86e62e1d5e7d161162f744d0c4517190a
silpi <silpighosh5907@gmail.com>
2025-10-27 15:13:40 +0530

    ok
    ok

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectdb = async() => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log(" mongodb connected");
+        console.log(" mongodb connected connected");
     } catch(err) {
         console.error(err);
     }
